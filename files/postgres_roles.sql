838cdc6f625fc1acc8db2b7f42cd4365bbd00361
Davide Polli <davide.polli@noema.it>
2023-11-28 15:45:23 +0100

    removed unused file

--- files/postgres_roles.sql
+++
@@ -1,9 +0,0 @@
-psql -p 5433
-CREATE ROLE pgpool WITH LOGIN; 
-CREATE ROLE repl WITH REPLICATION LOGIN; 
-\password pgpool ****** 
-\password postgres ****** 
-\password repl ***** 
-GRANT pg_monitor TO pgpool; 
-\c template1
-CREATE EXTENSION pgpool_recovery;
